5d424a7a515a92f896cdec8f95d2ea15d7f8bcd1
mkamburdev <muhammed.kambur@dci-student.org>
2024-02-22 03:37:05 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+function changeRed() {
+
+    let color = document.getElementById("background");
+    color.style.backgroundColor = "red";
+    color.style.color = "white";
+
+}
+
+function changeBlue() {
+
+    let color = document.getElementById("background");
+    color.style.backgroundColor = "blue";
+    color.style.color = "white";
+
+}
+
+function changeYellow() {
+
+    let color = document.getElementById("background");
+    color.style.backgroundColor = "yellow";
+    color.style.color = "black";
+
+}
+
+function changeBlack() {
+
+    let color = document.getElementById("background");
+    color.style.backgroundColor = "black";
+    color.style.color = "white";
+
+}
+
+function changeWhite() {
+
+    let color = document.getElementById("background");
+    color.style.backgroundColor = "white";
+    color.style.color = "black";
+
+}
